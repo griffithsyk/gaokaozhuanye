ea5296233aed3b9a4d5234f404f6c099c77f71a7
yksun <yksun@easipass.com>
2026-06-12 15:54:18 +0800

    fix: disable SSR for Naive UI compatibility

--- plugins/naive-ui.ts
+++ plugins/naive-ui.ts
@@ -2,5 +2,7 @@ import { defineNuxtPlugin } from '#app'
 import naive from 'naive-ui'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(naive)
+  if (import.meta.client) {
+    nuxtApp.vueApp.use(naive)
+  }
 })
